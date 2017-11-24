0fcd2d1d1ab6ec483d486efd6c85b1f809c4f10c
guoyongjian <1147657469@qq.com>
2017-11-24 23:06:42 +0800

    缓存设置

--- index.php
+++ index.php
@@ -6,7 +6,9 @@
  * Time: 20:41
  */
 
-include 'http.class.php';
-$http = new http('http://localhost/telnet.php?name=tom');
-$http->setHeader('Cookie:name=huozhe');
-$http->post();
+//include 'http.class.php';
+//$http = new http('http://localhost/telnet.php?name=tom');
+//$http->setHeader('Cookie:name=huozhe');
+//$http->post();
+
+echo 1>9?9|3:0|1|0|6;
